1252d7ac5d1b9480e83e8ba758e3bf1016a438d0
Simone Tripodi <simonetripodi@apache.org>
2011-05-13 10:12:21 +0000

    package HTM descriptor dropped in favour of Java5 descriptor
    git-svn-id: https://svn.apache.org/repos/asf/incubator/ognl/trunk@1102647 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/main/java/org/apache/commons/ognl/package-info.java
@@ -0,0 +1,37 @@
+/*
+ * $Id$
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+/**
+ * OGNL stands for Object-Graph Navigation Language; it is an expression language
+ * for getting and setting properties of Java objects. You use the same expression
+ * for both getting and setting the value of a property.
+ *
+ * OGNL started out as a way to set up associations between UI
+ * components and controllers using property names.  As the desire for
+ * more complicated associations grew, Drew Davidson created what he
+ * called KVCL, for Key-Value Coding Language, egged on by Luke
+ * Blanshard.  Luke then reimplemented the language using ANTLR, came up
+ * with the new name, and, egged on by Drew, filled it out to its current
+ * state.
+ *
+ * We pronounce OGNL as a word, like the last syllables of a drunken
+ * pronunciation of "orthogonal" or like "ogg-null".
+ */
+package org.apache.commons.ognl;
