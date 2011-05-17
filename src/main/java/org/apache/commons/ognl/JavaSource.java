8d95a52aef0319a08bf3cabdde2759650435ede0
Simone Tripodi <simonetripodi@apache.org>
2011-05-17 18:53:00 +0000

    relocated license header
    git-svn-id: https://svn.apache.org/repos/asf/incubator/ognl/trunk@1104466 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/commons/ognl/JavaSource.java
+++ src/main/java/org/apache/commons/ognl/JavaSource.java
@@ -1,5 +1,3 @@
-package org.apache.commons.ognl;
-
 /*
  * Licensed to the Apache Software Foundation (ASF) under one
  * or more contributor license agreements.  See the NOTICE file
@@ -18,6 +16,7 @@ package org.apache.commons.ognl;
  * specific language governing permissions and limitations
  * under the License.
  */
+package org.apache.commons.ognl;
 
 import org.apache.commons.ognl.enhance.ExpressionAccessor;
 
@@ -45,4 +44,5 @@ public interface JavaSource
      * @return Literal java string representation of an object get.
      */
     String toSetSourceString( OgnlContext context, Object target );
+
 }
