852e1d256fef3863dc41e603fb13ec9fa53b71ac
Simone Tripodi <simonetripodi@apache.org>
2011-05-17 19:27:20 +0000

    package located in the right place
    git-svn-id: https://svn.apache.org/repos/asf/incubator/ognl/trunk@1104483 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/commons/ognl/DefaultTypeConverter.java
+++ src/main/java/org/apache/commons/ognl/DefaultTypeConverter.java
@@ -1,3 +1,5 @@
+package org.apache.commons.ognl;
+
 /*
  * Licensed to the Apache Software Foundation (ASF) under one
  * or more contributor license agreements.  See the NOTICE file
@@ -16,7 +18,6 @@
  * specific language governing permissions and limitations
  * under the License.
  */
-package org.apache.commons.ognl;
 
 import java.lang.reflect.Member;
 import java.util.Map;
