905c4a076f5d91c31f2b3a4375f2cadc9e884110
Simone Tripodi <simonetripodi@apache.org>
2011-05-22 22:20:59 +0000

    added generated serialVersionUID
    git-svn-id: https://svn.apache.org/repos/asf/incubator/ognl/trunk@1126242 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/commons/ognl/InappropriateExpressionException.java
+++ src/main/java/org/apache/commons/ognl/InappropriateExpressionException.java
@@ -29,6 +29,9 @@ package org.apache.commons.ognl;
 public class InappropriateExpressionException
     extends OgnlException
 {
+
+    private static final long serialVersionUID = -101395753764475977L;
+
     public InappropriateExpressionException( Node tree )
     {
         super( "Inappropriate OGNL expression: " + tree );
