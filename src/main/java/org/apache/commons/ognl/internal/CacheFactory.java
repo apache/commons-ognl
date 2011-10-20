d99f167fa2efb79b1d16abc4b9d88e4aa95e653d
Maurizio Cucchiara <mcucchiara@apache.org>
2011-10-20 15:29:54 +0000

    Removed useless castes
    git-svn-id: https://svn.apache.org/repos/asf/commons/proper/ognl/branches/new-cache-approach@1186849 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/main/java/org/apache/commons/ognl/internal/CacheFactory.java
@@ -0,0 +1,44 @@
+/*
+ * $Id: $
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one  
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information       
+ * regarding copyright ownership.  The ASF licenses this file  
+ * to you under the Apache License, Version 2.0 (the           
+ * "License"); you may not use this file except in compliance  
+ * with the License.  You may obtain a copy of the License at  
+ *                                                             
+ *   http://www.apache.org/licenses/LICENSE-2.0                
+ *                                                             
+ * Unless required by applicable law or agreed to in writing,  
+ * software distributed under the License is distributed on an 
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY      
+ * KIND, either express or implied.  See the License for the   
+ * specific language governing permissions and limitations     
+ * under the License.
+ */
+
+package org.apache.commons.ognl.internal;
+
+import org.apache.commons.ognl.internal.entry.CacheEntryFactory;
+import org.apache.commons.ognl.internal.entry.ClassCacheEntryFactory;
+
+public class CacheFactory
+{
+
+    public static <K,V> Cache<K,V> createCache(CacheEntryFactory<K,V> entryFactory )
+    {
+        return new ConcurrentHashMapCache<K, V>(entryFactory );
+    }
+
+    public static <V> ClassCache<V> createClassCache( )
+    {
+        return createClassCache( null );
+    }
+
+    public static <V> ClassCache<V> createClassCache( ClassCacheEntryFactory<V> entryFactory )
+    {
+        return new ConcurrentClassCache<V>(entryFactory );
+    }
+}
