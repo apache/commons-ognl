b9b4a41048ca4fc4e99ee1ec743c2992eb815733
Maurizio Cucchiara <mcucchiara@apache.org>
2011-10-22 01:38:37 +0000

    Better interface where possible
    git-svn-id: https://svn.apache.org/repos/asf/commons/proper/ognl/branches/new-cache-approach@1187631 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/commons/ognl/OgnlRuntime.java
+++ src/main/java/org/apache/commons/ognl/OgnlRuntime.java
@@ -191,7 +191,7 @@ public class OgnlRuntime
 
     static final Map<String, Class<?>> _primitiveTypes = new HashMap<String, Class<?>>( 101 );
 
-    static final HashMap<Class<?>, Object> _primitiveDefaults = new HashMap<Class<?>, Object>( 20 );
+    static final Map<Class<?>, Object> _primitiveDefaults = new HashMap<Class<?>, Object>( 20 );
 
     static final Cache<Method, Class<?>[]> _methodParameterTypesCache =
         cacheFactory.createCache( new CacheEntryFactory<Method, Class<?>[]>( )
