96796985d6124fe73726e3799c4e6be7243400c7
Maurizio Cucchiara <mcucchiara@apache.org>
2011-10-25 15:09:55 +0000

    Fixed checkstyle warning (removed public keyword from interface)
    git-svn-id: https://svn.apache.org/repos/asf/commons/proper/ognl/trunk@1188697 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/commons/ognl/NodeVisitor.java
+++ src/main/java/org/apache/commons/ognl/NodeVisitor.java
@@ -1,3 +1,5 @@
+package org.apache.commons.ognl;
+
 /*
  * $Id: $
  *
@@ -18,54 +20,98 @@
  * specific language governing permissions and limitations
  * under the License.
  */
-package org.apache.commons.ognl;
 
 public interface NodeVisitor<R, P>
 {
-  public R visit(ASTSequence node, P data);
-  public R visit(ASTAssign node, P data);
-  public R visit(ASTTest node, P data);
-  public R visit(ASTOr node, P data);
-  public R visit(ASTAnd node, P data);
-  public R visit(ASTBitOr node, P data);
-  public R visit(ASTXor node, P data);
-  public R visit(ASTBitAnd node, P data);
-  public R visit(ASTEq node, P data);
-  public R visit(ASTNotEq node, P data);
-  public R visit(ASTLess node, P data);
-  public R visit(ASTGreater node, P data);
-  public R visit(ASTLessEq node, P data);
-  public R visit(ASTGreaterEq node, P data);
-  public R visit(ASTIn node, P data);
-  public R visit(ASTNotIn node, P data);
-  public R visit(ASTShiftLeft node, P data);
-  public R visit(ASTShiftRight node, P data);
-  public R visit(ASTUnsignedShiftRight node, P data);
-  public R visit(ASTAdd node, P data);
-  public R visit(ASTSubtract node, P data);
-  public R visit(ASTMultiply node, P data);
-  public R visit(ASTDivide node, P data);
-  public R visit(ASTRemainder node, P data);
-  public R visit(ASTNegate node, P data);
-  public R visit(ASTBitNegate node, P data);
-  public R visit(ASTNot node, P data);
-  public R visit(ASTInstanceof node, P data);
-  public R visit(ASTChain node, P data);
-  public R visit(ASTEval node, P data);
-  public R visit(ASTConst node, P data);
-  public R visit(ASTThisVarRef node, P data);
-  public R visit(ASTRootVarRef node, P data);
-  public R visit(ASTVarRef node, P data);
-  public R visit(ASTList node, P data);
-  public R visit(ASTMap node, P data);
-  public R visit(ASTKeyValue node, P data);
-  public R visit(ASTStaticField node, P data);
-  public R visit(ASTCtor node, P data);
-  public R visit(ASTProperty node, P data);
-  public R visit(ASTStaticMethod node, P data);
-  public R visit(ASTMethod node, P data);
-  public R visit(ASTProject node, P data);
-  public R visit(ASTSelect node, P data);
-  public R visit(ASTSelectFirst node, P data);
-  public R visit(ASTSelectLast node, P data);
+    R visit( ASTSequence node, P data );
+
+    R visit( ASTAssign node, P data );
+
+    R visit( ASTTest node, P data );
+
+    R visit( ASTOr node, P data );
+
+    R visit( ASTAnd node, P data );
+
+    R visit( ASTBitOr node, P data );
+
+    R visit( ASTXor node, P data );
+
+    R visit( ASTBitAnd node, P data );
+
+    R visit( ASTEq node, P data );
+
+    R visit( ASTNotEq node, P data );
+
+    R visit( ASTLess node, P data );
+
+    R visit( ASTGreater node, P data );
+
+    R visit( ASTLessEq node, P data );
+
+    R visit( ASTGreaterEq node, P data );
+
+    R visit( ASTIn node, P data );
+
+    R visit( ASTNotIn node, P data );
+
+    R visit( ASTShiftLeft node, P data );
+
+    R visit( ASTShiftRight node, P data );
+
+    R visit( ASTUnsignedShiftRight node, P data );
+
+    R visit( ASTAdd node, P data );
+
+    R visit( ASTSubtract node, P data );
+
+    R visit( ASTMultiply node, P data );
+
+    R visit( ASTDivide node, P data );
+
+    R visit( ASTRemainder node, P data );
+
+    R visit( ASTNegate node, P data );
+
+    R visit( ASTBitNegate node, P data );
+
+    R visit( ASTNot node, P data );
+
+    R visit( ASTInstanceof node, P data );
+
+    R visit( ASTChain node, P data );
+
+    R visit( ASTEval node, P data );
+
+    R visit( ASTConst node, P data );
+
+    R visit( ASTThisVarRef node, P data );
+
+    R visit( ASTRootVarRef node, P data );
+
+    R visit( ASTVarRef node, P data );
+
+    R visit( ASTList node, P data );
+
+    R visit( ASTMap node, P data );
+
+    R visit( ASTKeyValue node, P data );
+
+    R visit( ASTStaticField node, P data );
+
+    R visit( ASTCtor node, P data );
+
+    R visit( ASTProperty node, P data );
+
+    R visit( ASTStaticMethod node, P data );
+
+    R visit( ASTMethod node, P data );
+
+    R visit( ASTProject node, P data );
+
+    R visit( ASTSelect node, P data );
+
+    R visit( ASTSelectFirst node, P data );
+
+    R visit( ASTSelectLast node, P data );
 }
