80d3c2b28f172f6d066eb3c65edf158ed38e6fdd
Maurizio Cucchiara <mcucchiara@apache.org>
2011-10-29 17:42:47 +0000

    OGNL-11: fixed checkstyle errors (Fixed licence header position)
    git-svn-id: https://svn.apache.org/repos/asf/commons/proper/ognl/trunk@1194944 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/commons/ognl/internal/ClassCacheImpl.java
+++ src/main/java/org/apache/commons/ognl/internal/ClassCacheImpl.java
@@ -1,6 +1,6 @@
+package org.apache.commons.ognl.internal;
+
 /*
- * $Id$
- *
  * Licensed to the Apache Software Foundation (ASF) under one
  * or more contributor license agreements.  See the NOTICE file
  * distributed with this work for additional information
@@ -19,7 +19,9 @@
  * under the License.
  */
 
-package org.apache.commons.ognl.internal;
+/*
+ * $Id$
+ */
 
 import org.apache.commons.ognl.ClassCacheInspector;
 
