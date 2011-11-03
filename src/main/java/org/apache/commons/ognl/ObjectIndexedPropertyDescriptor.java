fc0c4119778602dec5d77b2c57359bef12f244b3
Maurizio Cucchiara <mcucchiara@apache.org>
2011-11-03 09:28:13 +0000

    OGNL-37 - Provide equals() and hashcode() implementation to override PropertyDescriptor.
    git-svn-id: https://svn.apache.org/repos/asf/commons/proper/ognl/trunk@1197018 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/commons/ognl/ObjectIndexedPropertyDescriptor.java
+++ src/main/java/org/apache/commons/ognl/ObjectIndexedPropertyDescriptor.java
@@ -118,4 +118,50 @@ public class ObjectIndexedPropertyDescriptor
     {
         return propertyType;
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o)
+        {
+            return true;
+        }
+
+        if (!(o instanceof ObjectIndexedPropertyDescriptor))
+        {
+            return false;
+        }
+
+        if (!super.equals(o))
+        {
+            return false;
+        }
+
+        ObjectIndexedPropertyDescriptor that = (ObjectIndexedPropertyDescriptor) o;
+
+        if (indexedReadMethod != null ? !indexedReadMethod.equals(that.indexedReadMethod) : that.indexedReadMethod != null)
+        {
+            return false;
+        }
+
+        if (indexedWriteMethod != null ? !indexedWriteMethod.equals(that.indexedWriteMethod) : that.indexedWriteMethod != null)
+        {
+            return false;
+        }
+
+        if (propertyType != null ? !propertyType.equals(that.propertyType) : that.propertyType != null)
+        {
+            return false;
+        }
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = super.hashCode();
+        result = 31 * result + (indexedReadMethod != null ? indexedReadMethod.hashCode() : 0);
+        result = 31 * result + (indexedWriteMethod != null ? indexedWriteMethod.hashCode() : 0);
+        result = 31 * result + (propertyType != null ? propertyType.hashCode() : 0);
+        return result;
+    }
 }
