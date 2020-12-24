83083297e82418cd19f2ddaa7bca68b12798b02a
Lukasz Lenart <lukaszlenart@apache.org>
2020-12-24 11:43:26 +0100

    Adjusts test to new JUnit lib

--- src/test/java/org/apache/commons/ognl/test/ASTChainTest.java
+++ src/test/java/org/apache/commons/ognl/test/ASTChainTest.java
@@ -27,23 +27,18 @@ import org.apache.commons.ognl.test.objects.IndexedSetObject;
 /**
  * Tests for {@link org.apache.commons.ognl.ASTChain}.
  */
-public class ASTChainTest
-    extends TestCase
-{
+public class ASTChainTest extends TestCase {
 
-    public void test_Get_Indexed_Value()
-        throws Exception
-    {
-
-        OgnlContext context = (OgnlContext) Ognl.createDefaultContext( null );
+    public void test_Get_Indexed_Value() throws Exception {
+        OgnlContext context = (OgnlContext) Ognl.createDefaultContext(null);
         IndexedSetObject root = new IndexedSetObject();
 
         String expr = "thing[\"x\"].val";
 
-        assertEquals( 1, Ognl.getValue( expr, context, root ) );
+        assertEquals(1, (int) Ognl.getValue(expr, context, root));
 
-        Ognl.setValue( expr, context, root, new Integer( 2 ) );
+        Ognl.setValue(expr, context, root, 2);
 
-        assertEquals( 2, Ognl.getValue( expr, context, root ) );
+        assertEquals(2, (int) Ognl.getValue(expr, context, root));
     }
 }
